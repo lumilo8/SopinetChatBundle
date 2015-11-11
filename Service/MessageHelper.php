f637bb000e03fd8441255657c58e2682124a2c19
Fernando Hidalgo <fhidalgo@sopinet.com>
2015-11-11 14:30:53 +0100

    Removed background system in test development

--- Service/MessageHelper.php
+++ Service/MessageHelper.php
@@ -131,7 +131,7 @@ class MessageHelper {
                 $em->flush();
 
                 // DO IN BACKGROUND
-                if ($config['background']) {
+                if ($config['background'] && $this->container->get('kernel')->getEnvironment() != 'test') {
                     $msg = array('messagePackageId' => $messagePackage->getId());
                     $this->container->get('old_sound_rabbit_mq.send_message_package_producer')->setContentType('application/json');
                     $this->container->get('old_sound_rabbit_mq.send_message_package_producer')->publish(json_encode($msg));
